8212c01ea6280d2d884156ce8600cb60559c2ce3
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-09 18:26:03 -0400

    add Base44 setup and project dependencies

---
+++ src/components/Home/CTABanner.jsx
@@ -0,0 +1,9 @@
+export default function CTABanner() {
+  return (
+    <section className="py-20 md:py-32">
+      <div className="max-w-7xl mx-auto px-6 md:px-16">
+        <div></div>
+      </div>
+    </section>
+  );
+}
